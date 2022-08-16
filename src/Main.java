5be4c13d0229a228248772e6e60763a096ee1b96
Ivanova <varia.nikitina@yandex.ru>
2022-08-16 23:09:30 +0600

    не могу понять почему 4 задача ругается на строку 92 . Третью исправила

--- src/Main.java
+++ src/Main.java
@@ -12,20 +12,20 @@ public class Main {
         // Самостоятельно выберите способ создания массива: с помощью ключевого слова или сразу заполненный элементами.
 
 
-        int[] container = new int[3];
-        container[0] = 1;
-        container[1] = 2;
-        container[2] = 3;
+        int[] containers = new int[3];
+        containers[0] = 1;
+        containers[1] = 2;
+        containers[2] = 3;
         double[] fractionalNumbers = {1.57, 7.654, 9.986};
-        int[] lemon = new int[]{5, 6, 8, 12, 27};
+        int[] lemons = new int[]{5, 6, 8, 12, 27};
 
         // Задача 2
         //Пройдите по каждому из трех массивов и распечатайте все элементы всех трех массивов,
         // начиная с первого элемента, через запятую. Запятая между последним элементом одного массива
         // и первым элементом следующего не нужна.
-        for (int i = 0; i < container.length; i++) {
-            System.out.print(container[i]);
-            if (i != container.length - 1) {
+        for (int i = 0; i < containers.length; i++) {
+            System.out.print(containers[i]);
+            if (i != containers.length - 1) {
                 System.out.print(",");
             }
         }
@@ -39,47 +39,49 @@ public class Main {
             }
         }
         System.out.println();
-        for (int i = 0; i < lemon.length; i++) {
-            System.out.print(lemon[i]);
-            if (i != lemon.length - 1) {
+        for (int i = 0; i < lemons.length; i++) {
+            System.out.print(lemons[i]);
+            if (i != lemons.length - 1) {
                 System.out.print(",");
 
+            }
+        }
+        System.out.println();
 
+        // Задача 3
+        //Теперь ваша задача – распечатать все элементы всех трех массивов,
+        // но начинать нужно не с первого элемента массива, а с последнего.
+        //Элементы должны быть распечатаны через запятую,
+        // при этом элементы одного массива располагаются на одной строчке,
+        // а элементы другого массива – на другой.
+        //Запятая между последним элементом одного массива и первым элементом следующего не нужна.
 
+        for (int a = containers.length - 1; a >= 0; a--) {
+            System.out.print(containers[a]);
+            if (a != 0) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int a = fractionalNumbers.length - 1; a >= 0; a--) {
+            System.out.print(fractionalNumbers[a]);
+            if (a != 0) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int a = lemons.length - 1; a >= 0; a--) {
+            System.out.print(lemons[a]);
+            if (a != 0) {
+                System.out.print(",");
+            }
+        }
 
+        System.out.println();
+        System.out.println();
+    }
 
-                // Задача 3
-                //Теперь ваша задача – распечатать все элементы всех трех массивов,
-                // но начинать нужно не с первого элемента массива, а с последнего.
-                //Элементы должны быть распечатаны через запятую,
-                // при этом элементы одного массива располагаются на одной строчке,
-                // а элементы другого массива – на другой.
-                //Запятая между последним элементом одного массива и первым элементом следующего не нужна.
-
-               for ( int a = container.length - 1; a >= 0; a--) {
-                    System.out.print(container[a]);
-                    if (a != 0) {
-                        System.out.print(",");
-                   }
-               }
-                System.out.println();
-                for ( int a = fractionalNumbers.length - 1; a >= 0; a--) {
-                    System.out.print(fractionalNumbers[a]);
-                    if (a != 0) {
-                        System.out.print(",");
-                    }
-               }
-                System.out.println();
-                for ( int a = lemon.length - 1; a >= 0; a--) {
-                    System.out.print(lemon[a]);
-                    if (a != 0) {
-                        System.out.print(",");
-                    }
-                    }
-
-                System.out.println();
-               System.out.println();
-
+}
                // Задача 4
                 //Пройдитесь по первому целочисленному массиву и все нечетные
                 // числа в нем сделайте четными (нужно прибавить 1).
@@ -87,22 +89,21 @@ public class Main {
                 // поэтому для решения задания вам нужно использовать циклы.
                 //Распечатайте результат преобразования в консоль.
 
-                for ( int b = 0 ; b<container.length; b++ ){
-                    if (container[b] % 2 == 1) {
-                        container [ b ]++;
+     //for ( int i = 0 ; i < containers.length; i++ ){
+    //if (containers[i] % 2 == 1) {
+   //   containers [ i ]++;
 
-                    }
-                    System.out.print( container[b]);
-                    if ( b!= container.length - 1) {
-                        System.out.print(",");
-                    }
-                }
+              //     }
+        //System.out.print(containers[i]);
+     //   if (i != containers.length - 1) {
+     //       System.out.print(",");
+    //    }
 
-            }
+   //      }
 
-        }
-    }
-}
+  //     }
+
+
 
 
 
